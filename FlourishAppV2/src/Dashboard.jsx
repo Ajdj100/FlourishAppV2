46c74fc5989c03461573dbc39ac4b33175903902
jasmine <116243241+imjas-mine@users.noreply.github.com>
2025-03-01 13:59:00 -0500

    built login and sign up page

---
+++ FlourishAppV2/src/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div className="h-screen w-screen bg-[#84C981] flex justify-center items-center">
+      <div className="flex w-[90%] h-[85%] bg-amber-50 shadow rounded-3xl overflow-hidden"></div>
+    </div>
+  );
+};
+
+export default Dashboard;
