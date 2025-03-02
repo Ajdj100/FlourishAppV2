5b52c3824c4ef138fab38ee002f8b4d1165f173b
jasmine <116243241+imjas-mine@users.noreply.github.com>
2025-03-01 19:36:32 -0500

    changed logout button and added function to fetch today's tasks from api

--- FlourishAppV2/src/Dashboard.jsx
+++ FlourishAppV2/src/Dashboard.jsx
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 const Dashboard = () => {
   const location = useLocation();
   const { username } = location.state || { username: "user" };
-  const userId=sessionStorage.getItem("userId");
+  const userId = sessionStorage.getItem("userId");
+  const [Tasks, setTasks] = useState([]);
 
-   
+  useEffect(() => {
+    const fetchTasks = async () => {
+      try {
+        const response = await fetch("http://10.144.112.144:8080/todaytasks", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ userId }),
+        });
+        if (response.ok) {
+          const data = await response.json();
+          setTasks(data);
+          console.log(Tasks);
+        } else {
+          throw new error("Failed to fetch");
+        }
+      } catch (e) {
+        console.error(e);
+      }
+    };
+  });
+
   return (
     <div className="h-screen w-screen bg-[#84C981] flex flex-col space-y-4 items-center p-3">
       <div className="w-[90%] bg-[#f1f1f1] h-[10%] rounded-3xl px-6 py-4 text-center flex justify-between  shadow-md shadow-green-900">
         <h1 className="text-3xl font-bold">Flourish🌿</h1>
-        <h1 className="text-xl cursor-pointer text-red-700 hover:font-bold">
+        <h1 className="text-xl cursor-pointer hover:underline">
           Logout
         </h1>
       </div>
@@ -20,11 +43,23 @@ const Dashboard = () => {
           <h1 className="text-2xl">
             Great job {username}👏, Your progress is blooming! ☘️
           </h1>
-          <div className="todo bg-[#f1f1f1] h-[40%] w-full rounded-2xl p-4 mt-3 shadow-md" style={{boxShadow: '0 4px 8px rgba(0, 0, 0, 0.5)' }}>
+          <div
+            className="todo bg-[#f1f1f1] h-[40%] w-full rounded-2xl p-4 mt-3 shadow-md"
+            style={{ boxShadow: "0 4px 8px rgba(0, 0, 0, 0.5)" }}
+          >
             <h1 className="text-xl ">My Today's Tasks</h1>
+            {/* {Tasks.map((task)=>{
+                <li>task.taskName</li>
+            })} */}
+            <div className="list-none ">
+
+            <li><input type="checkbox" />task 1</li>
+            <li><input type="checkbox" />task 2</li>
+            <li><input type="checkbox" />task 3</li>
+            </div>
           </div>
           <div className="mytasks p-4 mt-3">
-                <h1 className="text-xl ">My Tasks</h1>
+            <h1 className="text-xl ">My Tasks</h1>
           </div>
         </div>
         <div className="flex w-[51%] flex-col bg-[#e2e2e2d3] rounded-3xl overflow-hidden p-10 shadow-lg shadow-green-950">
